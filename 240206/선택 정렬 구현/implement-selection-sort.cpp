8cfb6ed670e0fc29d840a73f411e63bcb085e475
jaewon <139107488+jaerius@users.noreply.github.com>
2024-02-06 05:18:55 +0900

    Title: 선택 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/선택 정렬 구현/implement-selection-sort.cpp
+++ 240206/선택 정렬 구현/implement-selection-sort.cpp
@@ -16,7 +16,7 @@ int main() {
     for(int i=0;i<n;i++){
         int min;
         min = i;
-        for(int j=i+1; j<n+1; j++) {
+        for(int j=i+1; j<n; j++) {
             if(arr[j] < arr[min]){
                 int temp = arr[min];
                 arr[min] = arr[j];
