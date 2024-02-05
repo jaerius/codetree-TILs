b353c7601ca1e69e9977bcd97a397f25abfdd2c9
jaewon <139107488+jaerius@users.noreply.github.com>
2024-02-06 05:15:19 +0900

    Title: 선택 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240206/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+
+    for(int i=0;i<n;i++){
+        int min;
+        min = i;
+        for(int j=i+1; j<n+1; j++) {
+            if(arr[j] < arr[min]){
+                int temp = arr[min];
+                arr[min] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+    }
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
